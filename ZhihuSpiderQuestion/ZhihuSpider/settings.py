ce107db3828467c3eb7a15588d03f42f3df4217e
panchozheng <panchozheng@hotmail.com>
2015-05-18 21:17:53 +0800

    accomplish List All Questioins' ID

---
+++ ZhihuSpiderQuestion/ZhihuSpider/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for ZhihuSpider project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'ZhihuSpider'
+
+SPIDER_MODULES = ['ZhihuSpider.spiders']
+NEWSPIDER_MODULE = 'ZhihuSpider.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'ZhihuSpider (+http://www.yourdomain.com)'
